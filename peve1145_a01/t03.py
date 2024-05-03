066c25a0d1b06af88cb6ad745e25d195602f209e
RobertmPevec <robertmpevec@gmail.com>
2024-05-03 15:36:48 -0400

    fix

--- peve1145_a01/t03.py
+++ peve1145_a01/t03.py
@@ -1,2 +1,13 @@
+"""
+------------------------------------------------------------------------
+Assignment 1, Task 3
+------------------------------------------------------------------------
+Author: Robert Pevec
+ID:     169081145
+Email:  peve1145@mylaurier.ca
+__updated__ = '2024-04-02'
+------------------------------------------------------------------------
+"""
+
 from functions import dsmvwl
 print(dsmvwl('I think your book is an utter piece of garbage.'))
--- peve1145_a01/t04.py
+++ peve1145_a01/t04.py
@@ -1,3 +1,14 @@
+"""
+------------------------------------------------------------------------
+Assignment 1, Task 4
+------------------------------------------------------------------------
+Author: Robert Pevec
+ID:     169081145
+Email:  peve1145@mylaurier.ca
+__updated__ = '2024-04-02'
+------------------------------------------------------------------------
+"""
+
 from functions import file_analyze
 with open("t04.txt", "r", encoding="utf-8") as fh_1:
     # Call file_analyze function and pass the open file handle fh_1
